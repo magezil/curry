41b602856e6c380f3d5053d9526bd11591eac0ac
Liz Magalindan <256@holbertonschool.com>
2019-06-07 20:06:52 -0700

    Add code for curry

--- curry.js
+++ curry.js
@@ -1,7 +1,13 @@
-/* Implement Curry Here */
 
-
-
-
+function curry(func, args_param=[]) {
+    return function () {
+        const args = args_param.concat([...arguments]);
+        if (func.length <= args.length) {
+            temp = func.apply(null, args);
+            return temp;
+        }
+        return curry(func, args);
+    }
+}
 
 module.exports = curry;
